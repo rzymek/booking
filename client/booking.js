5432973033d6e05c4d4f624a078a79fa249600a6
rzymek <rzymek@gmail.com>
2013-11-05 19:45:11 +0100

    hoursConfigToDataURL

--- client/booking.js
+++ client/booking.js
@@ -1,7 +1,7 @@
 var workingHours = {
   mon: [],
   tue: [8, 16],
-  wed: [8,19],
+  wed: [8, 19],
   fri: [9, 17]
 };
 
@@ -32,30 +32,31 @@ Template.month.rendered = function () {
 };
 
 var canvas = document.createElement('canvas');
+
 function hoursToDataURL(arr) {
-  if(arr == null || arr.length == 0)
+  if (arr == null || arr.length == 0)
     return 'none';
   var ctx = canvas.getContext('2d');
   canvas.width = 10;
   canvas.height = 24;
   ctx.fillStyle = 'rgba(100,255,100,0.5)';
-  ctx.fillRect(0, arr[0], canvas.width, arr[1]-arr[0]);
-  return "url('"+canvas.toDataURL()+"')";  
+  ctx.fillRect(0, arr[0], canvas.width, arr[1] - arr[0]);
+  return "url('" + canvas.toDataURL() + "')";
+}
+
+function hoursConfigToDataURL(workingHours) {
+  var s = '';
+  for (var key in workingHours) {
+    var data = hoursToDataURL(workingHours[key]);
+    s += '#monthCal .fc-' + key + " { background-image: " + data + " }\n";
+  }
+  return s;
 }
 
 function showWorkingHours() {
-  console.log('showWorkingHours');
-  // $('.fc-day[data-date="2013-11-21"]').css('background-image', "url('"+data+"')"); 
   var styles = document.head.getElementsByTagName('style');
   var css = styles[styles.length - 1];
-  
-  var s='';
-  for(var key in workingHours) {
-    console.log(key);
-    var data = hoursToDataURL(workingHours[key]);
-    s+='#monthCal .fc-'+key+" { background-image: "+data+" }\n";
-  }
-  css.textContent = s;
+  css.textContent = hoursConfigToDataURL(workingHours);
 }
 
 Template.day.rendered = function () {
@@ -73,10 +74,10 @@ Template.day.rendered = function () {
     axisFormat: 'H:mm',
     defaultView: 'agendaDay'
   }, localOptions));
-  
+
   Deps.autorun(function () {
     cal.fullCalendar('option', 'contentHeight', Session.get('height'));
   });
-  
+
   Deps.autorun(showWorkingHours);
 };

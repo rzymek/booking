90b02ff5c7dde89349f158bb779b977b3bf11a29
rzymek <rzymek@gmail.com>
2013-11-05 19:24:59 +0100

    formatting

--- client/booking.js
+++ client/booking.js
@@ -1,75 +1,80 @@
 var workingHours = {
   mon: [],
-  tue: [8,16],
-  wed: [8,10],
-  wed: [12,18],
-  fri: ['8:15',17]
+  tue: [8, 16],
+  wed: [[8, 10], [12, 18]],
+  fri: ['8:15', 17]
 }
 
 var localOptions = {
-    buttonText: {
-        today: 'Dzisiaj',
-        month: 'Miesiąc',
-        day: 'Dzień',
-        week: 'Tydzień'
-    },
-    monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
-    monthNamesShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'],
-    dayNames: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
-    dayNamesShort: ['Nie', 'Pon', 'Wto', 'Śro', 'Czw', 'Pią', 'Sob'],
-    firstDay: 1
+  buttonText: {
+    today: 'Dzisiaj',
+    month: 'Miesiąc',
+    day: 'Dzień',
+    week: 'Tydzień'
+  },
+  monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+  monthNamesShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'],
+  dayNames: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+  dayNamesShort: ['Nie', 'Pon', 'Wto', 'Śro', 'Czw', 'Pią', 'Sob'],
+  firstDay: 1
 }
 Template.month.rendered = function () {
-
-    var cal = $('#monthCal');
-    cal.fullCalendar($.extend({
-        windowResize: function(view) {
-          Session.set('height', cal.height())
-        },
-        dayClick: function () {
-            console.log(this);
-        }        
-    }, localOptions));
-    Session.set('height', cal.height())
+  
+  var cal = $('#monthCal');
+  cal.fullCalendar($.extend({
+    windowResize: function (view) {
+      Session.set('height', cal.height())
+    },
+    dayClick: function () {
+      console.log(this);
+    }
+  }, localOptions));
+  Session.set('height', cal.height())
 }
 
 function getDynamicStyleSheet() {
   var sheets = document.head.getElementsByTagName('style');
-  for(var i=0;i<sheets.length;i++) {
-    if(sheets[i].title === 'dynamic') {
+  for (var i = 0; i < sheets.length; i++) {
+    if (sheets[i].title === 'dynamic') {
       return sheets[i];
     }
   }
 }
 
 function showWorkingHours() {
-      console.log('showWorkingHours');
-      var c = document.createElement('canvas');
-      var ctx = c.getContext('2d');
-      c.width = 10;
-      c.height = 24;
-      ctx.fillStyle = 'rgba(100,255,100,0.5)'  ;
-      ctx.fillRect(0, 8, c.width, 23-8);
-      var data = c.toDataURL();
-      // $('.fc-day[data-date="2013-11-21"]').css('background-image', "url('"+data+"')"); 
-      var styles = document.head.getElementsByTagName('style');
-      var css = styles[styles.length-1];
-      css.textContent = ".fc-wed { background-image: url('"+data+"') }";
+  console.log('showWorkingHours');
+  var c = document.createElement('canvas');
+  var ctx = c.getContext('2d');
+  c.width = 10;
+  c.height = 24;
+  ctx.fillStyle = 'rgba(100,255,100,0.5)';
+  ctx.fillRect(0, 8, c.width, 23 - 8);
+  var data = c.toDataURL();
+  // $('.fc-day[data-date="2013-11-21"]').css('background-image', "url('"+data+"')"); 
+  var styles = document.head.getElementsByTagName('style');
+  var css = styles[styles.length - 1];
+  css.textContent = ".fc-wed,.fc-mon { background-image: url('" + data + "') }";
 }
 
 Template.day.rendered = function () {
-    var cal = $('#dayCal');
-    cal.fullCalendar($.extend({
-        columnFormat: { day: 'dddd d.MM' },
-        allDaySlot:false,
-        header:{left:'', center:'', right:''},
-        axisFormat: 'H:mm',
-        defaultView: 'agendaDay'
-    }, localOptions));
-
-    Deps.autorun(function(){
-        cal.fullCalendar('option','contentHeight', Session.get('height'));
-    });
-
-    Deps.autorun(showWorkingHours);
+  var cal = $('#dayCal');
+  cal.fullCalendar($.extend({
+    columnFormat: {
+      day: 'dddd d.MM'
+    },
+    allDaySlot: false,
+    header: {
+      left: '',
+      center: '',
+      right: ''
+    },
+    axisFormat: 'H:mm',
+    defaultView: 'agendaDay'
+  }, localOptions));
+  
+  Deps.autorun(function () {
+    cal.fullCalendar('option', 'contentHeight', Session.get('height'));
+  });
+  
+  Deps.autorun(showWorkingHours);
 }
